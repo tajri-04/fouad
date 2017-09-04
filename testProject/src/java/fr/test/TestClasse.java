5a9528c2a4c3e19e1ec820a7aa26335e4aa26cf5
tajri <fouad.tajri@netapsys.fr>
2017-09-04 22:29:06 +0200

    comit and push

---
+++ testProject/src/java/fr/test/TestClasse.java
@@ -0,0 +1,15 @@
+package java.fr.test;
+
+public class TestClasse {
+
+	public static void main(String[] args) {
+		
+		
+		
+//		new comment
+		String myString="google";
+		System.out.println("my string rst"+ myString);
+
+	}
+
+}
